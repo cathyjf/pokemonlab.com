153ae9e403bc73e26b63cb765266a5ec5e2cfe45
Cathy Fitzpatrick <cathy@pokemonlab.com>
2013-01-02 04:48:44 +0000

    Revised licence page.

--- licence.php
+++ licence.php
@@ -27,13 +27,18 @@ require 'layout.php';
 
 <p>Shoddy Battle and Pok&eacute;mon Lab are &copy; 2006&ndash;2012 by Cathy Fitzpatrick, Benjamin Gwin, and others. See information on <a href="contributors">authors and contributors</a>.</p>
 
-<p>Shoddy Battle and Pok&eacute;mon Lab are <a href="http://www.gnu.org/philosophy/free-sw.html">free software</a> and open source; all of the source code is licensed under the terms of the <a href="http://www.fsf.org/licensing/licenses/agpl-3.0.html">GNU AGPL v3+</a> unless otherwise indicated. The Pok&eacute;mon Lab source code is available in the <a href="http://shoddybattle.hg.sf.net/hgweb/shoddybattle/">Mercurial repositories</a>. See the licence notices at the top of each individual file for details on the licence applicable to that file.</p>
+<p>Shoddy Battle and Pok&eacute;mon Lab are <a href="http://www.gnu.org/philosophy/free-sw.html">free software</a> and open source; all of the source code is licensed under the terms of the <a href="http://www.fsf.org/licensing/licenses/agpl-3.0.html">GNU AGPL v3+</a> unless otherwise indicated. The Pok&eacute;mon Lab source code is available in the <a href="http://shoddybattle.hg.sf.net/hgweb/shoddybattle/">Mercurial repositories</a>. See the licence notice at the top of each individual file for details on the licence applicable to that file.</p>
 
 <h2>pokemonlab.com</h2>
 
-<p>The <code>pokemonlab.com</code> web site is &copy; 2012 by Cathy Fitzpatrick.</p>
+<p>The <code>pokemonlab.com</code> web site is &copy; 2012&ndash;2013 by Cathy Fitzpatrick.</p>
 
 <p>In December 2012, <a href="contributors#Cathy">Cathy</a> wrote all the content for the present <code>pokemonlab.com</code> web site and designed an initial draft of the new layout. <a href="http://zarel.github.com/">Zarel</a> helped revise the layout.</p>
+
+<p>This web site is free. The source code for this web site is available in the <a href="http://shoddybattle.hg.sf.net/hgweb/shoddybattle/pokemonlab.com/"><code>pokemonlab.com</code>
+Mercurial repository</a>. See the licence notice at the top of each individual file for information on the licence applicable to that file. Most content on this web site is
+licensed under the <a href="http://creativecommons.org/licenses/by-sa/3.0">Creative Commons Attribution-ShareAlike 3.0 Unported License</a> ("CC-BY-SA"). If you use content from
+this web site purusant to the CC-BY-SA, you must attribute it to <code>pokemonlab.com</code>.</p>
 
 <p>The <code>pokemonlab.com</code> web site is also accessible under the domains <code>poke-lab.com</code> and <code>shoddybattle.com</code>.</p>
 
