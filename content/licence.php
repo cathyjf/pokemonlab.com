d853c34535b21914295b723647c31c0c92c9be5d
Cathy Fitzpatrick <cathy@pokemonlab.com>
2013-01-03 03:14:53 -0700

    Revised licence page.

--- content/licence.php
+++ content/licence.php
@@ -54,19 +54,9 @@ Shoddy Battle and Pok&eacute;mon Lab are <a href="http://www.gnu.org/philosophy/
 <table class="borderless">
 <tr>
 <td>
-<p>
 This web site is free. The source code for this web site is available in the <a href="http://shoddybattle.hg.sf.net/hgweb/shoddybattle/pokemonlab.com/"><code>pokemonlab.com</code>
 Mercurial repository</a>. See the licence notice at the top of each individual file for information on the licence applicable to that file. Most content on this web site is
-dual licensed under</p>
-
-<ol>
-<li>the <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a> ("CC-BY-SA-3U"), and</li>
-<li>the <a href="http://www.fsf.org/licensing/licenses/agpl-3.0.html">GNU Affero General Public License</a>, version 3 or later ("GAGPLv3+").</li>
-</ol>
-
-<p>
-You may use, modify, or redistribute files dual licensed in this manner pursuant to either the CC-BY-SA-3U or the GAGPLv3+, whichever licence is more convenient for you. If you use content from this web site pursuant to the CC-BY-SA-3U, you must attribute it to <code>pokemonlab.com</code>.
-</p>
+dual licensed under (1) the <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a> ("CC-BY-SA-3U"), and (2) the <a href="http://www.fsf.org/licensing/licenses/agpl-3.0.html">GNU Affero General Public License</a>, version 3 or later ("AGPLv3+"). You may use, modify, or redistribute files dual licensed in this manner pursuant to either the CC-BY-SA-3U or the AGPLv3+, whichever licence is more convenient for you. If you use content from this web site pursuant to the CC-BY-SA-3U, you must attribute it to <code>pokemonlab.com</code>.
 </td>
 <td style="padding-left: 10px;">
 <a href="http://creativecommons.org/licenses/by-sa/3.0/"><img alt="Creative Commons License" style="border-width: 0;" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" />
