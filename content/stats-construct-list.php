be00db82a12d9f1827d4647638d065856401573b
Cathy Fitzpatrick <cathy@pokemonlab.com>
2013-01-17 01:54:01 -0700

    Fix typo in JavaScript.

--- content/stats-construct-list.php
+++ content/stats-construct-list.php
@@ -34,7 +34,7 @@ include_once('setup-database.php');
 $q = mysql_query('SELECT DISTINCT month FROM stats ORDER BY month DESC');
 while (list($month) = mysql_fetch_row($q)) {
     echo "<li>$month (<a 
-        onmousedown=\"toggleSection(this); return false;\" 
+        onmousedown=\"toggleSection($(this)); return false;\" 
         href=\"#\">+</a>)<ul>";
     $ubers = mysql_num_rows(mysql_query("SELECT 
         DISTINCT ubers 
@@ -50,12 +50,12 @@ while (list($month) = mysql_fetch_row($q)) {
         $t = ($i == 0) ? 'Standard' : 'Ubers';
         $meta = ($i == 0) ? "false" : "true";
         echo "<li>$t (<a
-            onmousedown=\"toggleSection(this); return false;\"
+            onmousedown=\"toggleSection($(this)); return false;\"
             href=\"#\">+</a>)<ul>";
         foreach ($types as $j) {
             $weight = strtolower($j);
             echo "<li>$j (<a
-                onmousedown=\"loadSection($month, $meta, $weight, this); return false;\"
+                onmousedown=\"loadSection($month, $meta, $weight, $(this)); return false;\"
                 href=\"#\">+</a>)<ol>";
             echo '<li></li>'; // HTML does not allow empty lists
             echo '</ol></li>';
